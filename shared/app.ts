3dfccd3a8e43597aa826481be17f960f524775e3
XNeyMo <xneymodev@gmail.com>
2024-09-23 15:31:09 -0500

    si

--- shared/app.ts
+++ shared/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 import authRouter from '../routes/authRoutes';
 import taskRouter from '../routes/taskRoutes';
@@ -6,6 +7,9 @@ import taskRouter from '../routes/taskRoutes';
 const app = express();
 app.use(express.json());
 
+app.use(cors({
+	origin: 'http://localhost:3000',
+}));
 app.use(authRouter);
 app.use(taskRouter);
 
