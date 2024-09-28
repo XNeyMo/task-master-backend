8328b0078700cf5b668659a6dd4897fa12f8a47d
XNeyMo <xneymodev@gmail.com>
2024-09-28 16:43:54 -0500

    a

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -18,7 +18,7 @@ export const login = async (req: Request, res: Response) => {
 
 		const token = await createToken({ id: userFound._id });
 
-		res.cookie('token', token, { partitioned: true, httpOnly: true, sameSite: 'none', secure: true });
+		res.cookie('token', token, { partitioned: true, httpOnly: true, sameSite: 'none', secure: false });
 
 		res.json({
 			id: userFound._id,
