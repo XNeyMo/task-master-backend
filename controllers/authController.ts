a13378affef6947ad15d8795599712515a3be071
Neyan Montes <113221957+XNeyMo@users.noreply.github.com>
2024-09-28 19:08:59 -0500

    Update authController.ts

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -18,7 +18,7 @@ export const login = async (req: Request, res: Response) => {
 
 		const token = await createToken({ id: userFound._id });
 
-		res.cookie('token', token, { partitioned: true, httpOnly: true, sameSite: 'none', secure: true });
+		res.cookie('token', token, { partitioned: true, httpOnly: true, sameSite: 'none', secure: true, maxAge: 1000 * 60 * 60 * 24 * 7, });
 
 		res.json({
 			id: userFound._id,
